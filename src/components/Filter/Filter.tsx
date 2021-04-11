0c87d609b664c610e8f70d48971b2bb6e2840007
Tubagus <dutapanggulu@gmail.com>
2021-04-11 18:50:53 +0700

    add filter functionality

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useStyles from './styles';
-import {
-    Button,
-    FormControl,
-    Grid,
-    InputLabel,
-    MenuItem,
-    Select
-} from '@material-ui/core';
+import { Button, FormControl, Grid, InputLabel, MenuItem, Select } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/SearchRounded';
 
-function Filter() {
+interface FilterProps {
+    gsm: Array<number | ''>;
+    width: Array<number | ''>;
+    onFilter: (gsm: number | '', width: number | '') => void;
+}
+
+const Filter: React.FC<FilterProps> = ({ gsm, width, onFilter }) => {
     const classes = useStyles();
+    const [gsmValue, setGsmValue] = useState<number | ''>('');
+    const [widthValue, setWidthValue] = useState<number | ''>('');
+
+    const handleGSMChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+        setGsmValue(event.target.value as number);
+    };
+
+    const handleWidthChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+        setWidthValue(event.target.value as number);
+    };
+
+    const handleSubmit = () => {
+        onFilter(gsmValue, widthValue);
+    };
 
     return (
         <Grid container className={classes.filter} spacing={2}>
@@ -20,52 +33,12 @@ function Filter() {
                     <InputLabel shrink={true} id="GSM">
                         GSM
                     </InputLabel>
-                    <Select label="GSM">
-                        <MenuItem value={30}>30</MenuItem>
-                        <MenuItem value={35}>35</MenuItem>
-                        <MenuItem value={40}>40</MenuItem>
-                        <MenuItem value={45}>45</MenuItem>
-                        <MenuItem value={50}>50</MenuItem>
-                        <MenuItem value={55}>55</MenuItem>
-                        <MenuItem value={60}>60</MenuItem>
-                        <MenuItem value={64}>64</MenuItem>
-                        <MenuItem value={65}>65</MenuItem>
-                        <MenuItem value={30}>30</MenuItem>
-                        <MenuItem value={35}>35</MenuItem>
-                        <MenuItem value={40}>40</MenuItem>
-                        <MenuItem value={45}>45</MenuItem>
-                        <MenuItem value={50}>50</MenuItem>
-                        <MenuItem value={55}>55</MenuItem>
-                        <MenuItem value={60}>60</MenuItem>
-                        <MenuItem value={64}>64</MenuItem>
-                        <MenuItem value={65}>65</MenuItem>
-                        <MenuItem value={30}>30</MenuItem>
-                        <MenuItem value={35}>35</MenuItem>
-                        <MenuItem value={40}>40</MenuItem>
-                        <MenuItem value={45}>45</MenuItem>
-                        <MenuItem value={50}>50</MenuItem>
-                        <MenuItem value={55}>55</MenuItem>
-                        <MenuItem value={60}>60</MenuItem>
-                        <MenuItem value={64}>64</MenuItem>
-                        <MenuItem value={65}>65</MenuItem>
-                        <MenuItem value={30}>30</MenuItem>
-                        <MenuItem value={35}>35</MenuItem>
-                        <MenuItem value={40}>40</MenuItem>
-                        <MenuItem value={45}>45</MenuItem>
-                        <MenuItem value={50}>50</MenuItem>
-                        <MenuItem value={55}>55</MenuItem>
-                        <MenuItem value={60}>60</MenuItem>
-                        <MenuItem value={64}>64</MenuItem>
-                        <MenuItem value={65}>65</MenuItem>
-                        <MenuItem value={30}>30</MenuItem>
-                        <MenuItem value={35}>35</MenuItem>
-                        <MenuItem value={40}>40</MenuItem>
-                        <MenuItem value={45}>45</MenuItem>
-                        <MenuItem value={50}>50</MenuItem>
-                        <MenuItem value={55}>55</MenuItem>
-                        <MenuItem value={60}>60</MenuItem>
-                        <MenuItem value={64}>64</MenuItem>
-                        <MenuItem value={65}>65</MenuItem>
+                    <Select onChange={handleGSMChange} value={gsmValue} label="GSM">
+                        {gsm.map((value, index) => (
+                            <MenuItem key={index} value={value}>
+                                {value}
+                            </MenuItem>
+                        ))}
                     </Select>
                 </FormControl>
             </Grid>
@@ -74,33 +47,24 @@ function Filter() {
                     <InputLabel shrink={true} id="Ukuran">
                         Ukuran
                     </InputLabel>
-                    <Select label="Ukuran">
-                        <MenuItem value={'1000 MM'}>1000 MM</MenuItem>
-                        <MenuItem value={'1010 MM'}>1010 MM</MenuItem>
-                        <MenuItem value={'1020 MM'}>1020 MM</MenuItem>
-                        <MenuItem value={'1025 MM'}>1025 MM</MenuItem>
-                        <MenuItem value={'1030 MM'}>1030 MM</MenuItem>
-                        <MenuItem value={'1035 MM'}>1035 MM</MenuItem>
-                        <MenuItem value={'1050 MM'}>1050 MM</MenuItem>
-                        <MenuItem value={'1055 MM'}>1055 MM</MenuItem>
-                        <MenuItem value={'1060 MM'}>1060 MM</MenuItem>
+                    <Select onChange={handleWidthChange} value={widthValue} label="Ukuran">
+                        {width.map((value, index) => (
+                            <MenuItem key={index} value={value}>
+                                {value !== '' ? `${value} MM` : ''}
+                            </MenuItem>
+                        ))}
                     </Select>
                 </FormControl>
             </Grid>
             <Grid item xs={12} md={3}>
                 <span className={classes.searchButton}>
-                    <Button
-                        startIcon={<SearchIcon />}
-                        variant="contained"
-                        color="primary"
-                        disableElevation
-                    >
+                    <Button onClick={handleSubmit} startIcon={<SearchIcon />} variant="contained" color="primary" disableElevation>
                         Submit
                     </Button>
                 </span>
             </Grid>
         </Grid>
     );
-}
+};
 
 export default Filter;
--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -48,28 +48,32 @@ const columns: Column[] = [
 ];
 
 const rows = [
-    { id: 1, gsm: 11, width: 10, order: 12, item: 13, noMaterial: 14, lokasi: '55/1020' },
-    { id: 2, gsm: 11, width: 10, order: 12, item: 13, noMaterial: 14, lokasi: '55/1020' },
-    { id: 3, gsm: 11, width: 10, order: 12, item: 13, noMaterial: 14, lokasi: '55/1020' },
-    { id: 4, gsm: 11, width: 10, order: 12, item: 13, noMaterial: 14, lokasi: '55/1020' },
-    { id: 5, gsm: 11, width: 10, order: 12, item: 13, noMaterial: 14, lokasi: '55/1020' },
-    { id: 6, gsm: 11, width: 10, order: 12, item: 13, noMaterial: 14, lokasi: '55/1020' },
-    { id: 7, gsm: 11, width: 10, order: 12, item: 13, noMaterial: 14, lokasi: '55/1020' },
-    { id: 8, gsm: 11, width: 10, order: 12, item: 13, noMaterial: 14, lokasi: '55/1020' },
-    { id: 9, gsm: 11, width: 10, order: 12, item: 13, noMaterial: 14, lokasi: '55/1020' },
-    { id: 10, gsm: 11, width: 10, order: 12, item: 13, noMaterial: 14, lokasi: '55/1020' },
-    { id: 11, gsm: 11, width: 10, order: 12, item: 13, noMaterial: 14, lokasi: '55/1020' },
-    { id: 12, gsm: 11, width: 10, order: 12, item: 13, noMaterial: 14, lokasi: '55/1020' },
-    { id: 13, gsm: 11, width: 10, order: 12, item: 13, noMaterial: 14, lokasi: '55/1020' },
-    { id: 14, gsm: 11, width: 10, order: 12, item: 13, noMaterial: 14, lokasi: '55/1020' },
-    { id: 15, gsm: 11, width: 10, order: 12, item: 13, noMaterial: 14, lokasi: '55/1020' },
-    { id: 16, gsm: 11, width: 10, order: 12, item: 13, noMaterial: 14, lokasi: '55/1020' }
+    { id: 1, gsm: 50, width: 1010, order: 31036865, item: 1, noMaterial: 40413352, lokasi: '55/1' },
+    { id: 2, gsm: 50, width: 768, order: 31036867, item: 1, noMaterial: 40413353, lokasi: '56/5' },
+    { id: 3, gsm: 50, width: 1000, order: 31036868, item: 2, noMaterial: 40413354, lokasi: 'm4/12' },
+    { id: 4, gsm: 55, width: 1020, order: 31036869, item: 1, noMaterial: 40413355, lokasi: '55/8' },
+    { id: 5, gsm: 55, width: 1035, order: 31036810, item: 3, noMaterial: 40413356, lokasi: '55/2' },
+    { id: 6, gsm: 55, width: 650, order: 31036811, item: 10, noMaterial: 40413357, lokasi: '55/3' },
+    { id: 7, gsm: 55, width: 650, order: 31036812, item: 11, noMaterial: 40413358, lokasi: 'R2/2' },
+    { id: 8, gsm: 56, width: 1015, order: 31036813, item: 3, noMaterial: 40413359, lokasi: 'R3/3' },
+    { id: 9, gsm: 56, width: 780, order: 31036814, item: 2, noMaterial: 40413360, lokasi: 'R4/3' },
+    { id: 10, gsm: 57, width: 1020, order: 31036815, item: 4, noMaterial: 40413361, lokasi: '55/1' },
+    { id: 11, gsm: 57, width: 1020, order: 31036816, item: 4, noMaterial: 40413362, lokasi: '55/2' },
+    { id: 12, gsm: 57, width: 720, order: 31036817, item: 3, noMaterial: 40413363, lokasi: '55/1' },
+    { id: 13, gsm: 57, width: 900, order: 31036818, item: 2, noMaterial: 40413364, lokasi: 'R3/5' },
+    { id: 14, gsm: 60, width: 241, order: 31036819, item: 1, noMaterial: 40413365, lokasi: 'S3/5' },
+    { id: 15, gsm: 60, width: 642, order: 31036820, item: 6, noMaterial: 40413366, lokasi: 'S3/2' },
+    { id: 16, gsm: 60, width: 728, order: 31036821, item: 3, noMaterial: 40413367, lokasi: 'M4/23' }
 ];
+
+const gsm = [...new Set(rows.map((v) => v.gsm))];
+const width = [...new Set(rows.map((v) => v.width))];
 
 function Dashboard() {
     const classes = useStyles();
     const [page, setPage] = useState(0);
-    const [rowsPerPage, setRowsPerPage] = useState(10);
+    const [rowsPerPage, setRowsPerPage] = useState(25);
+    const [filteredRows, setFilteredRows] = useState([...rows]);
 
     const handleChangePage = (event: unknown, newPage: number) => {
         setPage(newPage);
@@ -80,9 +84,16 @@ function Dashboard() {
         setPage(0);
     };
 
+    const handleFilter = (gsm: number | '', width: number | '') => {
+        console.log(gsm, width);
+        if (gsm !== '' && width !== '') setFilteredRows((prev) => prev.filter((v) => v.gsm === gsm && v.width === width));
+        if (gsm !== '' && width === '') setFilteredRows((prev) => prev.filter((v) => v.gsm === gsm));
+        if (gsm === '' && width !== '') setFilteredRows((prev) => prev.filter((v) => v.width === width));
+    };
+
     return (
         <Paper className={classes.dashboard}>
-            <Filter />
+            <Filter onFilter={handleFilter} gsm={gsm} width={width} />
             <TableContainer className={classes.container}>
                 <Table stickyHeader aria-label="sticky table">
                     <TableHead>
@@ -95,11 +106,11 @@ function Dashboard() {
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
+                        {filteredRows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
                             return (
                                 <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
                                     <TableCell align="left">{row.gsm}</TableCell>
-                                    <TableCell align="left">{row.width}</TableCell>
+                                    <TableCell align="left">{row.width} MM</TableCell>
                                     <TableCell align="left">{row.order}</TableCell>
                                     <TableCell align="left">{row.item}</TableCell>
                                     <TableCell align="left">{row.noMaterial}</TableCell>
@@ -111,7 +122,7 @@ function Dashboard() {
                 </Table>
             </TableContainer>
             <TablePagination
-                rowsPerPageOptions={[10, 25, 100]}
+                rowsPerPageOptions={[25, 50, 100]}
                 labelRowsPerPage="rows/page"
                 component="div"
                 count={rows.length}
