c16e48714a1f8714476a2faebd2aafa3a334c514
Tubagus <dutapanggulu@gmail.com>
2021-04-12 18:57:23 +0700

    nuruti client

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -48,22 +48,22 @@ const columns: Column[] = [
 ];
 
 const rows = [
-    { id: 1, gsm: 50, width: 1010, order: 31036865, item: 1, noMaterial: 40413352, lokasi: '55/1' },
-    { id: 2, gsm: 50, width: 768, order: 31036867, item: 1, noMaterial: 40413353, lokasi: '56/5' },
-    { id: 3, gsm: 50, width: 1000, order: 31036868, item: 2, noMaterial: 40413354, lokasi: 'm4/12' },
-    { id: 4, gsm: 55, width: 1020, order: 31036869, item: 1, noMaterial: 40413355, lokasi: '55/8' },
-    { id: 5, gsm: 55, width: 1035, order: 31036810, item: 3, noMaterial: 40413356, lokasi: '55/2' },
-    { id: 6, gsm: 55, width: 650, order: 31036811, item: 10, noMaterial: 40413357, lokasi: '55/3' },
-    { id: 7, gsm: 55, width: 650, order: 31036812, item: 11, noMaterial: 40413358, lokasi: 'R2/2' },
-    { id: 8, gsm: 56, width: 1015, order: 31036813, item: 3, noMaterial: 40413359, lokasi: 'R3/3' },
-    { id: 9, gsm: 56, width: 780, order: 31036814, item: 2, noMaterial: 40413360, lokasi: 'R4/3' },
-    { id: 10, gsm: 57, width: 1020, order: 31036815, item: 4, noMaterial: 40413361, lokasi: '55/1' },
-    { id: 11, gsm: 57, width: 1020, order: 31036816, item: 4, noMaterial: 40413362, lokasi: '55/2' },
-    { id: 12, gsm: 57, width: 720, order: 31036817, item: 3, noMaterial: 40413363, lokasi: '55/1' },
-    { id: 13, gsm: 57, width: 900, order: 31036818, item: 2, noMaterial: 40413364, lokasi: 'R3/5' },
-    { id: 14, gsm: 60, width: 241, order: 31036819, item: 1, noMaterial: 40413365, lokasi: 'S3/5' },
-    { id: 15, gsm: 60, width: 642, order: 31036820, item: 6, noMaterial: 40413366, lokasi: 'S3/2' },
-    { id: 16, gsm: 60, width: 728, order: 31036821, item: 3, noMaterial: 40413367, lokasi: 'M4/23' }
+    { id: 1, gsm: 55, width: 1020, order: 31036865, item: 1, noMaterial: 40413352, lokasi: 'A1' },
+    { id: 2, gsm: 55, width: 1020, order: 31036867, item: 1, noMaterial: 40413353, lokasi: 'A2' },
+    { id: 3, gsm: 55, width: 1020, order: 31036868, item: 2, noMaterial: 40413354, lokasi: 'A3' },
+    { id: 4, gsm: 55, width: 1020, order: 31036869, item: 1, noMaterial: 40413355, lokasi: 'B1' },
+    { id: 5, gsm: 55, width: 1020, order: 31036810, item: 3, noMaterial: 40413356, lokasi: 'B2' },
+    { id: 6, gsm: 55, width: 1020, order: 31036811, item: 10, noMaterial: 40413357, lokasi: 'B3' },
+    { id: 7, gsm: 55, width: 1020, order: 31036812, item: 11, noMaterial: 40413358, lokasi: 'B4' },
+    { id: 8, gsm: 55, width: 1020, order: 31036813, item: 3, noMaterial: 40413359, lokasi: 'B5' },
+    { id: 9, gsm: 55, width: 1020, order: 31036814, item: 2, noMaterial: 40413360, lokasi: 'C1' },
+    { id: 10, gsm: 55, width: 1020, order: 31036815, item: 4, noMaterial: 40413361, lokasi: 'C2' },
+    { id: 11, gsm: 55, width: 1020, order: 31036816, item: 4, noMaterial: 40413362, lokasi: 'C3' },
+    { id: 12, gsm: 55, width: 1020, order: 31036817, item: 3, noMaterial: 40413363, lokasi: 'C4' },
+    { id: 13, gsm: 55, width: 1020, order: 31036818, item: 2, noMaterial: 40413364, lokasi: 'D1' },
+    { id: 14, gsm: 55, width: 1020, order: 31036819, item: 1, noMaterial: 40413365, lokasi: 'D2' },
+    { id: 15, gsm: 55, width: 1020, order: 31036820, item: 6, noMaterial: 40413366, lokasi: 'D3' },
+    { id: 16, gsm: 55, width: 1020, order: 31036821, item: 3, noMaterial: 40413367, lokasi: 'D4' }
 ];
 
 const gsm = [...new Set(rows.map((v) => v.gsm))];
@@ -73,7 +73,7 @@ function Dashboard() {
     const classes = useStyles();
     const [page, setPage] = useState(0);
     const [rowsPerPage, setRowsPerPage] = useState(25);
-    const [filteredRows, setFilteredRows] = useState([...rows]);
+    const [filteredRows, setFilteredRows] = useState<typeof rows>([]);
 
     const handleChangePage = (event: unknown, newPage: number) => {
         setPage(newPage);
@@ -125,7 +125,7 @@ function Dashboard() {
                 rowsPerPageOptions={[25, 50, 100]}
                 labelRowsPerPage="rows/page"
                 component="div"
-                count={rows.length}
+                count={filteredRows.length}
                 rowsPerPage={rowsPerPage}
                 page={page}
                 onChangePage={handleChangePage}
